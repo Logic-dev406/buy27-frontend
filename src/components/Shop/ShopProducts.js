2f6a4cd265d6d04dda75080d89dc8d3aef892ed7
patrick sunday <sundayblinkz1@gmail.com>
2021-06-09 23:18:42 +0100

    added number formater to shop page

--- src/components/Shop/ShopProducts.js
+++ src/components/Shop/ShopProducts.js
@@ -1,45 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import NumberFormat from "react-number-format";
 
 const ShopProducts = ({ products, loading, error }) => {
-    return (
-        <div>
-            <div className="  bg-transparent md:bg-white">
-                <div className=" grid grid-cols-2 md:grid-cols-4 gap-y-2 md:gap-y-2 gap-x-2 md:gap-x-2 py-2 md:py-2 px2 md:px-2 bg-transparent">
-                    {loading ? (
-                        <h1>Loading...</h1>
-                    ) : error ? (
-                        <h2>{error}</h2>
-                    ) : (
-                        products.map((product) => (
-                            <Link
-                                to={`/Shop/${product.slug}`}
-                                key={product.id}
-                                className=" bg-white * hover:shadow-lg h-68 md:h-80 w-36 md:w-44 rounded justify-center "
-                            >
-                                <img
-                                    className="h-40 md:h-48 w-36 md:w-44 rounded-t"
-                                    src={product.image}
-                                    alt="product"
-                                />
-                                <h1 className="text-left text-black text-xs font-semibold pl-2 pt-2">
-                                    {product.name}
-                                </h1>
+  return (
+    <div>
+      <div className="  bg-transparent md:bg-white">
+        <div className=" grid grid-cols-2 md:grid-cols-4 gap-y-2 md:gap-y-2 gap-x-2 md:gap-x-2 py-2 md:py-2 px2 md:px-2 bg-transparent">
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : error ? (
+            <h2>{error}</h2>
+          ) : (
+            products.map((product) => (
+              <Link
+                to={`/Shop/${product.slug}`}
+                key={product.id}
+                className=" bg-white * hover:shadow-lg h-68 md:h-80 w-36 md:w-44 rounded justify-center "
+              >
+                <img
+                  className="h-40 md:h-48 w-36 md:w-44 rounded-t"
+                  src={product.image}
+                  alt="product"
+                />
+                <h1 className="text-left text-black text-xs font-semibold pl-2 pt-2">
+                  {product.name}
+                </h1>
 
-                                <h5 className="text-left text-gray-500 text-xs pl-2 font-semibold">
-                                    <span className="mr-1">&#8358;</span>
-                                    {product.price}
-                                </h5>
-                                <button className="text-sm font-normal md:font-bold mb-2 md:mb-0 mt-4 md:mt-10 mx-0 md:mx-2 ml-2 bg-primary-dark md:bg-transparent hover:bg-primary-dark rounded border border-primary-dark text-white md:text-primary-dark hover:text-white w-32 md:w-40 h-8 md:h-10 focus:outline-none">
-                                    View Product
-                                </button>
-                            </Link>
-                        ))
-                    )}
-                </div>
-            </div>
+                <h5 className="text-left text-gray-500 text-xs pl-2 font-semibold">
+                  <NumberFormat
+                    value={product.price}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"₦"}
+                  />
+                </h5>
+                <button className="text-sm font-normal md:font-bold mb-2 md:mb-0 mt-4 md:mt-10 mx-0 md:mx-2 ml-2 bg-primary-dark md:bg-transparent hover:bg-primary-dark rounded border border-primary-dark text-white md:text-primary-dark hover:text-white w-32 md:w-40 h-8 md:h-10 focus:outline-none">
+                  View Product
+                </button>
+              </Link>
+            ))
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShopProducts;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,70 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import ShopProduct from '../components/Shop/ShopProducts';
-import ScrollToTop from '../components/Shop/ScrollToTop';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import ShopProduct from "../components/Shop/ShopProducts";
+import ScrollToTop from "../components/Shop/ScrollToTop";
+import { useSelector, useDispatch } from "react-redux";
 
 //Action
-import { getProducts as listProducts } from '../redux/actions/productActions';
+import { getProducts as listProducts } from "../redux/actions/productActions";
 
 const Shop = ({
-    searchLoading,
-    searchError,
-    searchedProducts,
-    getSearchedProducts,
+  searchLoading,
+  searchError,
+  searchedProducts,
+  getSearchedProducts,
 }) => {
-    const [isMobile, setisMobile] = useState(
-        window.matchMedia('(max-width:768px)').matches
-    );
+  const [isMobile, setisMobile] = useState(
+    window.matchMedia("(max-width:768px)").matches
+  );
 
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            setisMobile(window.matchMedia('(max-width:768px)').matches);
-        });
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setisMobile(window.matchMedia("(max-width:768px)").matches);
     });
+  });
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const getProducts = useSelector((state) => state.getProducts);
-    const { products, loading, error } = getProducts;
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
 
-    useEffect(() => {
-        dispatch(listProducts());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    const [didMount, setDidMount] = useState(false);
+  const [didMount, setDidMount] = useState(false);
 
-    useEffect(() => {
-        setDidMount(true);
-        return () => setDidMount(false);
-    }, []);
+  useEffect(() => {
+    setDidMount(true);
+    return () => setDidMount(false);
+  }, []);
 
-    if (!didMount) {
-        return null;
-    }
+  if (!didMount) {
+    return null;
+  }
 
-    return (
-        <div className="flex flex-col bg-gray-100 h-full w-full">
-            <div className="flex justify-center mx-0 md:mx-52">
-                <div>
-                    {isMobile ? (
-                        ''
-                    ) : (
-                        <div className="h-96 w-72 bg-white mr-5"></div>
-                    )}
-                </div>
-                <ShopProduct
-                    products={products}
-                    loading={loading}
-                    error={error}
-                    searchedProducts={searchedProducts}
-                    searchError={searchError}
-                    searchLoadin={searchLoading}
-                    getSearchedProducts={getSearchedProducts}
-                />
-                <div>{isMobile ? '' : <ScrollToTop />}</div>
-            </div>
+  return (
+    <div className="flex flex-col bg-gray-100 h-full w-full">
+      <div className="flex justify-center mx-0 md:mx-52">
+        <div>
+          {isMobile ? "" : <div className="h-96 w-72 bg-white mr-5"></div>}
         </div>
-    );
+        <ShopProduct
+          products={products}
+          loading={loading}
+          error={error}
+          searchedProducts={searchedProducts}
+          searchError={searchError}
+          searchLoadin={searchLoading}
+          getSearchedProducts={getSearchedProducts}
+        />
+        <div>{isMobile ? "" : <ScrollToTop />}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
