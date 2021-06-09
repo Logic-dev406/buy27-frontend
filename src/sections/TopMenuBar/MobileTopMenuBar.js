939dc81a91fccd98cb82948e43a4f3eff3a77388
patrick sunday <sundayblinkz1@gmail.com>
2021-06-09 20:34:19 +0100

    updated the login system for mobile view

--- src/sections/TopMenuBar/MobileTopMenuBar.js
+++ src/sections/TopMenuBar/MobileTopMenuBar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import MenuIcon from "@material-ui/icons/Menu";
 // import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import ShoppingBasketOutlinedIcon from "@material-ui/icons/ShoppingBasketOutlined";
@@ -12,9 +11,15 @@ import WatchOutlinedIcon from "@material-ui/icons/WatchOutlined";
 import WbIncandescentOutlinedIcon from "@material-ui/icons/WbIncandescentOutlined";
 import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
 import HomeWorkOutlinedIcon from "@material-ui/icons/HomeWorkOutlined";
+import PhoneOutlinedIcon from "@material-ui/icons/PhoneOutlined";
+import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
+import { useDispatch } from "react-redux";
+
+import { logout } from "../../redux/actions/authAction";
 
 const MobileTopMenuBar = () => {
   const [active, setActive] = useState(false);
+  const dispatch = useDispatch();
   const user = JSON.parse(window.localStorage.getItem("user"));
 
   return (
@@ -30,31 +35,32 @@ const MobileTopMenuBar = () => {
             <MenuIcon />
           </button>
           {active && (
-            <div className="flex flex-col absolute text-primary-dark font-semibold w-full h-full bg-white py-2 text-sm">
+            <div className="flex flex-col absolute text-primary-dark font-semibold w-full h-full bg-white py-2 text-sm overflow-y-scroll ">
               <div className=" flex flex-col justify-center px-4 items-start py-2 w-full bg-transparent">
                 {user ? (
                   <div>
-                    <div className="flex">
+                    <div className="flex mb-2">
                       <div className=" flex justify-center items-center h-10 w-10 mr-4 rounded bg-primary-dark">
-                        <h1 className="text-white text-lg font-semibold">S</h1>
+                        <h1 className="text-white text-lg font-semibold">
+                          {user ? user.firstName.charAt(0) : null}
+                        </h1>
                       </div>
                       <div>
                         <h1 className="text-primary-dark font-bold text-sm ">
-                          Sunday Patrick
+                          {user.firstName} {user.lastName}
                         </h1>
-                        <h2 className="text-primary-dark  ">
-                          sundaypatrick406@gmail.com
-                        </h2>
+                        <h2 className="text-primary-dark  ">{user.email}</h2>
                       </div>
                     </div>
-                    <Link className=" pt-4 " to="#">
-                      <h1 className="text-primary-dark text-xs font-semibold">
-                        ACCOUNT SETTINGS
-                      </h1>
-                    </Link>
+                    <a
+                      className="text-primary-dark text-xs font-semibold "
+                      href="/Dashboard"
+                    >
+                      ACCOUNT SETTINGS
+                    </a>
                   </div>
                 ) : (
-                  <div className="flex justify-between my-4 mx-4">
+                  <div className="flex w-full items-center justify-between my-4 ">
                     <a
                       href="/login"
                       className=" py-1 px-10 rounded bg-transparent border border-2 border-primary-dark text-primary-dark  "
@@ -103,11 +109,20 @@ const MobileTopMenuBar = () => {
                 <WbIncandescentOutlinedIcon /> Lighting
               </a>
               <a href="/" className="px-4 py-2 border-t border-gray-300">
-                Contact Us
+                <PhoneOutlinedIcon /> Contact Us
               </a>
-              <a href="/" className="px-4 py-2">
-                About Us
-              </a>
+              {user ? (
+                <button
+                  onClick={() => {
+                    dispatch(logout());
+                    window.location.reload(false);
+                    return setActive(!active);
+                  }}
+                  className="flex px-4 py-2 font-semibold"
+                >
+                  <ExitToAppOutlinedIcon /> Logout
+                </button>
+              ) : null}
             </div>
           )}
         </>
