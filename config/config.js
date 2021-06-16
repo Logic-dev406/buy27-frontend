7223383046624caeea4e7abe0afe81175c56d7bd
patrick sunday <sundayblinkz1@gmail.com>
2021-06-16 13:23:20 +0100

    added paystack publickey to .env variables

--- config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  paystack: {
-    publicKey: process.env.PAYSTACK_TEST_PUBLIC_KEY,
-  },
-};
---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  LiveBackendUrl: process.env.REACT_APP_LIVE_BACKEND_URL,
+  LocalBackendUrl: process.env.REACT_APP_LOCAL_BACKEND_URL,
+
+  paystack: {
+    publicKey: process.env.PAYSTACK_TEST_PUBLIC_KEY,
+  },
+};
